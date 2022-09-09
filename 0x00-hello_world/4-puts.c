cf5e8d4a886bdee3aeb252b36046392fa7c0928a
sirhmvfx <sirhmvfx@gmail.com>
2022-09-08 19:07:16 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,20 @@
-#include<studio.h>
+#include<stdio.h>
+
 /**
- * main - Entry point
  *
- * Return: Always ) (Success)
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+			return (0);
+
 }
