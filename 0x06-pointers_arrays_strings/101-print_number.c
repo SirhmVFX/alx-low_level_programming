886481c5ad1c59ce820545b7774ef10e7d283f19
sirhmvfx <sirhmvfx@gmail.com>
2022-09-21 09:45:47 -0700

    numbers have life

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/** 
+ * main - a function that prints an integer
+ * you are not allowed to use long
+ * you are not allowed to use arrays or pointers
+ * you are not allowed to hard-code special values
+ */ 
+void print_number(int n)
+{
+	int n; 
+	n = 100;
+
+	printf("here is an integer : %d", n);
+
+	return 0;
+}
